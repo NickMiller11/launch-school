abe31c6e0c08af7a5108fe0e68c9e9cb71661d8a
Nick Miller <nick.miller00@gmail.com>
2017-10-06 11:14:36 -0700

    Preparing for SD

--- ls101/small_problems/easy5/alphabetical_numbers.rb
+++ ls101/small_problems/easy5/alphabetical_numbers.rb
@@ -5,10 +5,28 @@
 # zero, one, two, three, four, five, six, seven, eight, nine, ten, eleven,
 # twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen
 
+NUMBERS = {
+'one'=>1, 'two'=>2, 'three'=>3, 'four'=>4, 'five'=>5, 'six'=>6, 'seven'=>7,
+'eight'=>8, 'nine'=>9, 'ten'=>10, 'eleven'=>11, 'twelve'=>12, 'thirteen'=>13,
+'fourteen'=>14, 'fifteen'=>15, 'sixteen'=>16, 'seventeen'=>17, 'eighteen'=>18,
+'nineteen'=>19, 'zero'=>0
+}
 
+def alphabetic_number_sort(array)
+  num_keys = NUMBERS.keys.sort
+  final_array = []
+  num_keys.each { |num| final_array << NUMBERS[num] }
+  final_array
+end
 
+# Input: array
+# Data structure: hash
+# Output: array
 
-alphabetic_number_sort((0..19).to_a) == [
+# grab the keys, put them in alphabetical order, and use them to call
+# the corresponding value
+
+p alphabetic_number_sort((0..19).to_a) == [
   8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,
   6, 16, 10, 13, 3, 12, 2, 0
 ]
--- ls101/small_problems/easy5/letter_counter_part2.rb
+++ ls101/small_problems/easy5/letter_counter_part2.rb
@@ -2,11 +2,21 @@
 # non-letters when determining word size.
 # For instance, the length of "it's" is 3, not 4.
 
+def word_sizes(string)
+  string.gsub!(/[^a-z ]/i, "")
+  arr = string.split(' ')
+  hsh = Hash.new(0)
+  arr.each { |word| hsh[word.length] += 1 }
+  hsh
+end
 
+# just like part 1, except I'm going to use that fancy pancy new regex to replace
+# anything not an alphabetical character or a space with nothing (i.e., remove it).
 
+# after checking the LS solution, it looks like we can use the delete
+# method here too
 
-
-word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
-word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
-word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
-word_sizes('') == {}
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
+p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
+p word_sizes('') == {}
