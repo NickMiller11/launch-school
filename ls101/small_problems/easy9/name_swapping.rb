6a74c84532da63a88f2c05390ccded2dbdb886a5
Nick Miller <nick.miller00@gmail.com>
2017-10-17 17:59:42 +0000

    cloud sync

--- ls101/small_problems/easy9/name_swapping.rb
+++ ls101/small_problems/easy9/name_swapping.rb
@@ -4,7 +4,17 @@
 # a comma, a space, and the first name.
 
 def swap_name(full_name)
-  
+  arr = full_name.split
+  arr[1] += ','
+  arr[0], arr[1] = arr[1], arr[0]
+  arr.join(' ')
 end
 
-swap_name('Joe Roberts') == 'Roberts, Joe'
+def swap_name_ls(full_name)
+  full_name.split.reverse.join(', ')
+end
+
+
+p swap_name('Joe Roberts') == 'Roberts, Joe'
+
+p swap_name_ls('Joe Roberts') == 'Roberts, Joe'
