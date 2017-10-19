b000151cb6e881361582c283614e948948ca8d03
Nick Miller <nick.miller00@gmail.com>
2017-10-19 13:20:06 -0700

    air sync

--- ls101/small_problems/medium1/thousand_lights.rb
+++ ls101/small_problems/medium1/thousand_lights.rb
@@ -1,12 +1,12 @@
-# You have a bank of switches before you, numbered from 1 to n. 
-# Each switch is connected to exactly one light that is initially off. 
-# You walk down the row of switches and toggle every one of them. 
-# You go back to the beginning, and on this second pass, you toggle 
-# switches 2, 4, 6, and so on. On the third pass, you go back again 
-# to the beginning and toggle switches 3, 6, 9, and so on. You repeat 
+# You have a bank of switches before you, numbered from 1 to n.
+# Each switch is connected to exactly one light that is initially off.
+# You walk down the row of switches and toggle every one of them.
+# You go back to the beginning, and on this second pass, you toggle
+# switches 2, 4, 6, and so on. On the third pass, you go back again
+# to the beginning and toggle switches 3, 6, 9, and so on. You repeat
 # this process and keep going until you have been through n repetitions.
 
-# Write a method that takes one argument, the total number of switches, 
+# Write a method that takes one argument, the total number of switches,
 # and returns an Array that identifies which lights are on after n repetitions.
 
 # Example with n = 5 lights:
@@ -16,17 +16,59 @@
 # round 3: lights 2, 3, and 4 are now off; 1 and 5 are on
 # round 4: lights 2 and 3 are now off; 1, 4, and 5 are on
 # round 5: lights 2, 3, and 5 are now off; 1 and 4 are on
-# The result is that 2 lights are left on, lights 1 and 4. 
+# The result is that 2 lights are left on, lights 1 and 4.
 # The return value is [1, 4].
 
 # With 10 lights, 3 lights are left on: lights 1, 4, and 9. The return value is [1, 4, 9].
 
-def thousand_lights(switch_qty)
-  lights = (1..switch_qty).each_with_object({}) { |num, hash| hash[num] = true }
+require "pry"
+
+def initialize_lights(number)
+  Array.new(number + 1, false)
 end
+
+def thousand_lights(switch_qty)
+  lights = initialize_lights(switch_qty)
+  counter = 1
+  final_lights = []
+  loop do
+    break if counter == switch_qty + 1
+    lights.map!.with_index do |num, i|
+      i % counter == 0 ? !num : num
+    end
+    # binding.pry
+    counter += 1
+  end
+  lights.index(true)  # I'm right here
+end
+
+
+# input: integer
+# data structure: array
+# output: array
+
+p thousand_lights(5)
 
 # input: integer
 # data structure: hash with boolean
 # output: array
 
-p thousand_lights(5)
+# def initialize_lights(number)
+#   (1..number).each_with_object({}) { |num, hash| hash[num] = false }
+# end
+#
+# def toggle_lights(lights, number, round)
+#   new_state = {}
+#   lights.each do |bulb, state|
+#     # binding.pry
+#     bulb % round == 0 ? new_state[bulb] = true : new_state[bulb] = false
+#   end
+#   new_state
+# end
+#
+# def thousand_lights(switch_qty)
+#   lights = initialize_lights(switch_qty)
+#   1.upto(switch_qty) do |round|
+#     toggle_lights(lights, switch_qty, round)
+#   end
+# end
