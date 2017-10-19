ffc6c468e23ccbde07a0b611524b79a4a71a78ff
Nick Miller <nick.miller00@gmail.com>
2017-10-19 19:12:42 +0000

    cloud sync

--- ls101/small_problems/medium1/rotation_part2.rb
+++ ls101/small_problems/medium1/rotation_part2.rb
@@ -3,28 +3,17 @@
 # You may use the rotate_array method from the previous exercise if you want.
 # You may assume that n is always a positive integer.
 
-require 'pry'
 
 def rotate_rightmost_digits(num, index)
   arr = num.digits.reverse
-  removed_digit = -index
-  arr.delete_at(removed_digit)
+  selected_digit = arr.delete_at(-index)
+  arr.push(selected_digit).join('').to_i
 end
 
-p rotate_rightmost_digits(735291, 1) # == 735291
-p rotate_rightmost_digits(735291, 2) # == 735219
-p rotate_rightmost_digits(735291, 3) # == 735912
-p rotate_rightmost_digits(735291, 4) # == 732915
-p rotate_rightmost_digits(735291, 5) # == 752913
-p rotate_rightmost_digits(735291, 6) # == 352917
+p rotate_rightmost_digits(735291, 1)  == 735291
+p rotate_rightmost_digits(735291, 2)  == 735219
+p rotate_rightmost_digits(735291, 3)  == 735912
+p rotate_rightmost_digits(735291, 4)  == 732915
+p rotate_rightmost_digits(735291, 5)  == 752913
+p rotate_rightmost_digits(735291, 6)  == 352917
 
-# def rotate_rightmost_digits(num, index)
-#   arr = num.digits.reverse
-#   corrected_index = -index - 1
-#   binding.pry
-#   if index == 1
-#     arr[0..corrected_index] + arr[corrected_index + 1..-1]
-#   else
-#     arr[0..corrected_index] + arr[corrected_index + 1..-1] + [arr[-index]]
-#   end
-# end
