f183a5bb9c7252c0d2e731f0bb198644bbfdcbcd
Nick Miller <nick.miller00@gmail.com>
2017-10-23 20:31:12 -0700

    desk sync

--- ls101/small_problems/medium1/word_to_digit.rb
+++ ls101/small_problems/medium1/word_to_digit.rb
@@ -23,19 +23,30 @@ require "pry"
 NUMBERS = {'zero'=>0, 'one'=>1, 'two'=>2, 'three'=>3, 'four'=>4, 'five'=>5,
   'six'=>6, 'seven'=>7, 'eight'=>8, 'nine'=>9 }
 
+DIGIT_HASH = {'zero'=>'0', 'one'=>'1', 'two'=>'2', 'three'=>'3', 'four'=>'4',
+  'five'=>'5', 'six'=>'6', 'seven'=>'7', 'eight'=>'8', 'nine'=>'9' }.freeze
+
 def word_to_digit(str)
   arr = str.split(' ')
   arr.map! do |val|
-    if NUMBERS.keys.include?(val)
-      NUMBERS[val]
-      binding.pry
+    if NUMBERS.keys.include?(val.gsub(/[^a-z]/, ''))
+      val = NUMBERS[val.gsub(/[^a-z]/, '')]
+      # binding.pry
     else
       val
     end
   end
-  arr.join
+  arr.join(' ')
+end
+
+def word_to_digit_ls(words)
+  DIGIT_HASH.keys.each do |word|
+    words.gsub!(/\b#{word}\b/, DIGIT_HASH[word])
+  end
+  words
 end
 
 
-p word_to_digit('Please call me at five five five one two three four. Thanks.') #==
+p word_to_digit('Please call me at five five five one two three four. Thanks.')
+p word_to_digit_ls('Please call me at five five five one two three four. Thanks.') #==
 #'Please call me at 5 5 5 1 2 3 4. Thanks.'
