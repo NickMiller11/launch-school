7aefadc55e0708a17413572cbdb8050e5566e661
Nick Miller <nick.miller00@gmail.com>
2017-10-29 15:42:19 -0700

    air sync

--- ls101/small_problems/medium2/bubble_sort.rb
+++ ls101/small_problems/medium2/bubble_sort.rb
@@ -7,7 +7,7 @@
 # On each pass, each pair of consecutive elements is compared. If the first of
 # the two elements is greater than the second, then the two elements are swapped.
 # This process is repeated until a complete pass is made without performing any
-# swaps; at that point, the Array is completely sorted.rations) through the Array. 
+# swaps; at that point, the Array is completely sorted.rations) through the Array.
 
 # Write a method that takes an Array as an argument, and sorts that Array using
 # the bubble sort algorithm as just described. Note that your sort will be
@@ -16,18 +16,84 @@
 
 =begin
 
+Input: array
+Data structure: iteration
+Output: array
 
+So I'm definitely going to have to iterate through the array several times.
+I'm going to have to loop, and then break only when no changes have been made.
+
+I'm going to need a way to determine if there are no more changes while iterating.
+- I could compare the array each time with the array from the last pass.
+- I could also use a counter that I increment each time a change happens, then
+break if the counter remains zero after the full pass.  Counter is reset each pass
+
+I could also push the items to an intermediate array if mutating the original
+array doesn't go as planned
+
+Steps:
+Set up a loop
+Check arr[0] and arr[1].
+If arr[0] is larger, switch with arr[1]
+Check arr[1] and arr[2].
+....
+Break loop if no more changes have been made
+Start at beginning again
+Return modified array
 
 =end
 
-array = [5, 3]
-bubble_sort!(array)
-array == [3, 5]
+require "pry"
+
+def bubble_sort!(arr)
+
+  loop do
+    counter = 0
+    times_bubbled = 0
+    loop do
+      compare = arr[counter] <=> arr[counter + 1]
+      if compare == 1
+        arr[counter], arr[counter + 1] = arr[counter + 1], arr[counter]
+        times_bubbled += 1
+      end
+      counter += 1
+      break if counter == arr.size - 1
+    end
+    break if times_bubbled == 0
+  end
+  arr
+end
+
+# Below code does not work
+
+# def bubble_sort!(arr)
+#   loop do
+#     times_bubbled = 0
+#     arr.map!.with_index do |num, i|
+#       compare = arr[i] <=> arr[i + 1]
+#       if compare == 1
+#         times_bubbled += 1
+#         arr[i], arr[i + 1] = arr[i + 1], arr[i]
+#       else
+#         arr[i]
+#       end
+#     end
+#     break if times_bubbled == 0
+#   end
+#   arr
+# end
+
+
 
 array = [6, 2, 7, 1, 4]
-bubble_sort!(array)
-array == [1, 2, 4, 6, 7]
+p bubble_sort!(array)
+# array == [1, 2, 4, 6, 7]
+
+array = [5, 3]
+p bubble_sort!(array)
+# array == [3, 5]
+
 
 array = %w(Sue Pete Alice Tyler Rachel Kim Bonnie)
-bubble_sort!(array)
-array == %w(Alice Bonnie Kim Pete Rachel Sue Tyler)
+p bubble_sort!(array)
+# array == %w(Alice Bonnie Kim Pete Rachel Sue Tyler)
--- ls101/small_problems/medium2/sum_square.rb
+++ ls101/small_problems/medium2/sum_square.rb
@@ -1,11 +1,35 @@
-# Write a method that computes the difference between the square of 
+# Write a method that computes the difference between the square of
 # the sum of the first n positive integers and the sum of the squares
 # of the first n positive integers.
 
+=begin
 
+Input: integer
+Output: integer
 
-sum_square_difference(3) == 22
+This can be broken down into two pieces; the left side and the right side
+of the equation
+
+After fiddling around in IRB, I figured out the left side can be solved by
+assigning the following to a variable and then squaring that variable
+(1..num).inject(:+)
+
+The right side of the equation can be solved with:
+(1..3).inject { |sum, v| sum + v**2 }
+
+I'm going to assign each side of the equation to a variable and subtract them
+
+=end
+
+def sum_square_difference(int)
+  squared_sum = (1..int).inject(:+)
+  squared_sum = squared_sum**2
+  sum_squared = (1..int).inject { |sum,v| sum + v**2 }
+  squared_sum - sum_squared
+end
+
+p sum_square_difference(3) #== 22
    # -> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
-sum_square_difference(10) == 2640
-sum_square_difference(1) == 0
-sum_square_difference(100) == 25164150
+p sum_square_difference(10) #== 2640
+p sum_square_difference(1) #== 0
+p sum_square_difference(100) #== 25164150
