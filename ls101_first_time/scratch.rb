e9d92c4d0deb0becef0fb98f2fa1bd92403989f4
Nick Miller <nick.miller00@gmail.com>
2017-11-12 10:14:42 -0500

    air sync

---
+++ ls101_first_time/scratch.rb
@@ -0,0 +1,19 @@
+words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+          'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+          'flow', 'neon']
+
+results = {}
+
+words.each do |word|
+  key = word.split('').sort.join
+  if results.include?(key)
+    results[key].push(word)
+  else
+    results[key] = [word]
+  end
+end
+
+results.each do |k,v|
+  puts v
+  puts "------"
+end
---
+++ ls101_second_time/scratch_paper.rb
@@ -0,0 +1,6 @@
+NUMBERS = [1, 2, 3]
+def test
+  puts NUMBERS.inspect
+end
+
+test
