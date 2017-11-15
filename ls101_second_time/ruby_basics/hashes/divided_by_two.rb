fc1e4aa48570de08467b8897dfda11d3d02560eb
Nick Miller <nick.miller00@gmail.com>
2017-11-15 12:14:32 -0800

    air sync

---
+++ ls101_second_time/ruby_basics/hashes/divided_by_two.rb
@@ -0,0 +1,11 @@
+# Use Hash#map to iterate over numbers and return an array
+# containing each number divided by 2. Assign the returned
+# array to a variable named half_numbers and print its value using #p.
+
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
+p numbers.map { |k,v| v / 2 }
