c44cea437069072e1601589df3374446e616fcc9
Nick Miller <nick.miller00@gmail.com>
2017-11-15 21:10:36 -0800

    air sync

--- ls101_second_time/small_problems/easy_2/how_big_is_room.rb
+++ ls101_second_time/small_problems/easy_2/how_big_is_room.rb
@@ -1,5 +1,5 @@
-# Build a program that asks a user for the length and width of a room 
-# in meters and then displays the area of the room in both square meters 
+# Build a program that asks a user for the length and width of a room
+# in meters and then displays the area of the room in both square meters
 # and square feet.
 
 # Note: 1 square meter == 10.7639 square feet
@@ -24,5 +24,21 @@ Rule:
 - ask for dimensions of a room
 
 Algorithm:
+- ask user for length of room, assign to a variable
+- ask user for width of room, assign to a variable
+- convert meters to feet
+- print string inserting square meters and square feet (floats)
 
 =end
+
+METERS_TO_FEET = 10.7639
+
+puts("Enter the length of the room in feet:")
+length = gets.chomp.to_f
+puts("Enter the width of the room in feet:")
+width = gets.chomp.to_f
+square_feet = (length * width).round(2)
+square_inches = (square_feet * 144).round(2)
+square_centimeters = (square_feet * 9.29).round(2)
+puts("The area of the room is #{square_feet} square feet" +
+  " (#{square_inches} square inches and #{square_centimeters} square centimeters).")
