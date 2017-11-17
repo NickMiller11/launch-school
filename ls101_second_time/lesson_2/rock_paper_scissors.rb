a73748776f8813f6d653f427e539bc6e5c4ce0af
Nick Miller <nick.miller00@gmail.com>
2017-11-16 21:35:26 -0800

    desk sync

--- ls101_second_time/lesson_2/rock_paper_scissors.rb
+++ ls101_second_time/lesson_2/rock_paper_scissors.rb
@@ -1,4 +1,5 @@
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = %w((r)ock (p)aper (s)cissors (l)izard (sp)ock)
+COMPUTER_CHOICES = %w(r p s l sp)
 
 def prompt(message)
   puts("=> #{message}")
@@ -7,7 +8,14 @@ end
 def win?(first, second)
   (first == 'rock' && second == 'scissors') ||
     (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+    (first == 'scissors' && second == 'paper') ||
+    (first == 'rock' && second == 'lizard') ||
+    (first == 'lizard' && second == 'spock') ||
+    (first == 'spock' && second == 'scissors') ||
+    (first == 'scissors' && second == 'lizard') ||
+    (first == 'lizard' && second == 'paper') ||
+    (first == 'paper' && second == 'spock') ||
+    (first == 'spock' && second == 'rock')
 end
 
 def display_results(player, computer)
@@ -20,28 +28,56 @@ def display_results(player, computer)
   end
 end
 
+def convert_choice(player)
+  case player
+  when 'r'
+    'rock'
+  when 'p'
+    'paper'
+  when 's'
+    'scissors'
+  when 'l'
+    'lizard'
+  else
+    'spock'
+  end
+end
+
+player_wins = 0
+computer_wins = 0
+
 loop do
   choice = ''
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
     choice = gets.chomp
 
-    if VALID_CHOICES.include?(choice)
+    if VALID_CHOICES.each { |selection| selection[0..3].include?(choice) }
       break
     else
       prompt("That's not a valid choice.")
     end
   end
 
-  computer_choice = VALID_CHOICES.sample
+  computer_choice = COMPUTER_CHOICES.sample
 
-  puts("You chose: #{choice}; Computer chose: #{computer_choice}")
+  puts("You chose: #{convert_choice(choice)};" +
+       " Computer chose: #{convert_choice(computer_choice)}")
 
-  display_results(choice, computer_choice)
+  display_results(convert_choice(choice), convert_choice(computer_choice))
+
+  case
+  when win?(convert_choice(choice), convert_choice(computer_choice))
+    player_wins += 1
+  when win?(convert_choice(computer_choice), convert_choice(choice))
+    computer_wins += 1
+  end
+
+  display_score() # need to create this method
 
   prompt("Do you want to play again?")
   answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  break unless answer.downcase.start_with?('y') || (player_wins == 5 || computer_wins == 5)
 end
 
 prompt("Thank you for playing. Good bye!")
