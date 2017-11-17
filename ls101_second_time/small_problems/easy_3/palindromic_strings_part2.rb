28902cba569270ed86c00868c14419baf14c7043
Nick Miller <nick.miller00@gmail.com>
2017-11-17 12:44:03 -0800

    air sync

--- ls101_second_time/small_problems/easy_3/palindromic_strings_part2.rb
+++ ls101_second_time/small_problems/easy_3/palindromic_strings_part2.rb
@@ -1,6 +1,6 @@
-# Write another method that returns true if the string passed as an argument is a 
-# palindrome, false otherwise. This time, however, your method should be 
-# case-insensitive, and it should ignore all non-alphanumeric characters. 
+# Write another method that returns true if the string passed as an argument is a
+# palindrome, false otherwise. This time, however, your method should be
+# case-insensitive, and it should ignore all non-alphanumeric characters.
 # If you wish, you may simplify things by calling the palindrome? method you
 # wrote in the previous exercise.
 
@@ -17,7 +17,8 @@ def palindrome?(input)
 end
 
 def real_palindrome?(input)
-  
+  input.gsub!(/[^a-z]/i, '')
+  palindrome?(input.downcase)
 end
 
 
