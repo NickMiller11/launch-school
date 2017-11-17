54cd51680695a6d40f71ec68686eb6c090767d37
Nick Miller <nick.miller00@gmail.com>
2017-11-17 19:41:28 +0000

    cloud sync

--- ls101_second_time/small_problems/easy_2/mutation.rb
+++ ls101_second_time/small_problems/easy_2/mutation.rb
@@ -7,12 +7,14 @@ array1.each { |value| array2 << value }
 puts array1[0].object_id
 puts array2[0].object_id
 array1.each { |value| value.upcase! if value.start_with?('C', 'S') }
-# puts array2
+puts array2
 
 =begin
 
-The following code will print exactly what array1 started out as.
-This is because although array1 and array2 are pointing to identical strings,
-They are not the same string
+The following code will print the array with the strings Larry, Shemp, and Chico 
+all in caps.
+
+This is because the objects at each index location share the same object id, so
+mutations to those objects will persist between collections.
 
 =end
