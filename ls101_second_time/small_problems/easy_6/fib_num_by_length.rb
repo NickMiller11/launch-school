72fbcdd563ae67d017d916c6b47afab4084944a1
Nick Miller <nick.miller00@gmail.com>
2017-11-24 13:55:09 -0800

    desk sync

---
+++ ls101_second_time/small_problems/easy_6/fib_num_by_length.rb
@@ -0,0 +1,13 @@
+# The Fibonacci series is a series of numbers (1, 1, 2, 3, 5, 8, 13, 21, ...)
+# such that the first 2 numbers are 1 by definition, and each subsequent number
+# is the sum of the two previous numbers. This series appears throughout the
+# natural world.
+#
+# Computationally, the Fibonacci series is a very simple series, but the results
+# grow at an incredibly rapid rate. For example, the 100th Fibonacci number is
+# 354,224,848,179,261,915,075 -- that's enormous, especially considering that it
+# takes 6 iterations before it generates the first 2 digit number.
+#
+# Write a method that calculates and returns the index of the first Fibonacci
+# number that has the number of digits specified as an argument. (The first
+#   Fibonacci number has index 1.)
