0b5370d560cbf72e6f9a28c8fff0d83289c5ef86
Nick Miller <nick.miller00@gmail.com>
2017-11-25 15:44:01 -0800

    air sync

---
+++ ls101_second_time/small_problems/easy_7/lettercase_counter.rb
@@ -0,0 +1,4 @@
+# Write a method that takes a string, and then returns a hash that contains 3 
+# entries: one represents the number of characters in the string that are
+# lowercase letters, one the number of characters that are uppercase letters,
+# and one the number of characters that are neither.
