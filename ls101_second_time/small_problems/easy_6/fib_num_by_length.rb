2d686dce5402c04221222fd986dd798b3e97d8dc
Nick Miller <nick.miller00@gmail.com>
2017-11-25 09:10:30 -0800

    air sync

--- ls101_second_time/small_problems/easy_6/fib_num_by_length.rb
+++ ls101_second_time/small_problems/easy_6/fib_num_by_length.rb
@@ -11,3 +11,41 @@
 # Write a method that calculates and returns the index of the first Fibonacci
 # number that has the number of digits specified as an argument. (The first
 #   Fibonacci number has index 1.)
+
+=begin
+
+Input: Integer
+Output: Integer
+
+Rules:
+- return index where first number of input length appears
+
+Algorithm:
+- Use a loop to calculate Fib number
+- Push result to array
+- Compare result length vs input (to_s + length)
+- Return index if it meets the criterea (starting at 1)
+
+=end
+
+def find_fibonacci_index_by_length(size)
+  num1 = 1
+  num2 = 1
+  temp_num = 0
+  fib_array = [num1, num2]
+  loop do
+    temp_num = num2
+    num2 += num1
+    num1 = temp_num
+    fib_array << num2
+    break if num2.to_s.size == size
+  end
+  fib_array.index(num2) + 1
+end
+
+
+p find_fibonacci_index_by_length(2) == 7
+p find_fibonacci_index_by_length(10) == 45
+p find_fibonacci_index_by_length(100) == 476
+p find_fibonacci_index_by_length(1000) == 4782
+p find_fibonacci_index_by_length(10000) == 47847
