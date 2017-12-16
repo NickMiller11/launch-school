9b0699f101bd1892287ba5d1c9fc958451992ce1
Nick Miller <nick.miller00@gmail.com>
2017-12-16 15:42:21 -0800

    desk sync

---
+++ ls120/small_problems_take_two/easy2/nobility.rb
@@ -0,0 +1,40 @@
+# Now that we have a Walkable module, we are given a new challenge.
+# Apparently some of our users are nobility, and the regular way of
+# walking simply isn't good enough. Nobility need to strut.
+#
+# We need a new class Noble that shows the title and name when walk is called:
+
+module Walkable
+  def walk
+    puts "#{name} #{gait} forward"
+  end
+end
+
+class Noble
+  attr_reader :name
+
+  include Walkable
+
+  def initialize(name, title)
+    @name = title + ' ' + name
+  end
+
+  def gait
+    "struts"
+  end
+end
+
+
+byron = Noble.new("Byron", "Lord")
+p byron.walk
+# => "Lord Byron struts forward"
+
+# We must have access to both name and title because they are needed for
+# other purposes that we aren't showing here.
+
+byron.name
+# => "Byron"
+byron.title
+# => "Lord"
+
+# -- still not finished
