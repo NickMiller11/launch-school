99c5197e95e0a349be8d7ff96f23905808b63727
Nick Miller <nick.miller00@gmail.com>
2017-12-19 17:40:33 -0800

    air sync

--- ls120/small_problems_take_two/medium1/stack_machine_interpretation.rb
+++ ls120/small_problems_take_two/medium1/stack_machine_interpretation.rb
@@ -5,16 +5,16 @@
 # n Place a value n in the "register". Do not modify the stack.
 # PUSH Push the register value on to the stack. Leave the value in the register.
 # ADD Pops a value from the stack and adds it to the register value, storing the result in the register.
-# SUB Pops a value from the stack and subtracts it from the register value, 
-# storing the result in the register. 
-
-# MULT Pops a value from the stack and multiplies it by the register value, 
+# SUB Pops a value from the stack and subtracts it from the register value,
 # storing the result in the register.
 
-# DIV Pops a value from the stack and divides it into the register value, 
-# storing the integer result in the register. 
+# MULT Pops a value from the stack and multiplies it by the register value,
+# storing the result in the register.
 
-# MOD Pops a value from the stack and divides it into the register value, 
+# DIV Pops a value from the stack and divides it into the register value,
+# storing the integer result in the register.
+
+# MOD Pops a value from the stack and divides it into the register value,
 # storing the integer remainder of the division in the register.
 
 # POP Remove the topmost item from the stack and place in register
@@ -23,10 +23,10 @@
 
 # All operations are integer operations (which is only important with DIV and MOD).
 
-# Programs will be supplied to your language method via a string passed in as an 
-# argument. Your program should produce an error if an unexpected item is present 
-# in the string, or if a required stack value is not on the stack when it should 
-# be (the stack is empty). In all error cases, no further processing should be 
+# Programs will be supplied to your language method via a string passed in as an
+# argument. Your program should produce an error if an unexpected item is present
+# in the string, or if a required stack value is not on the stack when it should
+# be (the stack is empty). In all error cases, no further processing should be
 # performed on the program.
 
 # You should initialize the register to 0.
@@ -36,16 +36,16 @@ require 'set'
 class MinilangRuntimeError < RuntimeError; end
 class BadTokenError < MinilangRuntimeError; end
 class EmptyStackError < MinilangRuntimeError; end
-  
+
 class Minilang
   ACTIONS = Set.new %w(PUSH ADD SUB MULT DIV MOD POP PRINT)
-  
+
   def initialize(program)
     @program = program
     @stack = []
     @register = 0
   end
-  
+
   def eval
     @program.split.each { |token| eval_token(token) }
   rescue MinilangRuntimeError => error
@@ -53,8 +53,51 @@ class Minilang
   end
 
   private
-  
-  
+
+  def eval_token(token)
+    if ACTIONS.include?(token)
+      send(token.downcase)
+    elsif token =~ /\A[-+]?\d+\z/
+      @register = token.to_i
+    else
+      raise BadTokenError, "Invalid token: #{token}"
+    end
+  end
+
+  def push
+    @stack.push(@register)
+  end
+
+  def pop
+    raise EmptyStackError, "Empty stack!" if @stack.empty?
+    @register = @stack.pop
+  end
+
+  def add
+    @register += pop
+  end
+
+  def div
+    @register /= pop
+  end
+
+  def mod
+    @register %= pop
+  end
+
+  def mult
+    @register *= pop
+  end
+
+  def sub
+    @register -= pop
+  end
+
+  def print
+    puts @register
+  end
+end
+
 
 
 
