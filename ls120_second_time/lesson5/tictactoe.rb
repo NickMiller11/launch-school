81d3096c2bee9f4c0d4f0df066a7fd02e7afbc23
Nick Miller <nick.miller00@gmail.com>
2018-01-08 14:28:57 -0800

    air sync

--- ls120_second_time/lesson5/tictactoe.rb
+++ ls120_second_time/lesson5/tictactoe.rb
@@ -1,6 +1,6 @@
 =begin
 
-Tic Tac Toe is a 2-player board game played on a 3x3 grid.  
+Tic Tac Toe is a 2-player board game played on a 3x3 grid.
 Players take turns marking a square.
 The first player to mark 3 squares in a row wins.
 
@@ -15,43 +15,120 @@ Player
 
 =end
 
+require 'pry'
+
 class Board
+  INITIAL_MARKER = ' '
+
   def initialize
-  # need some way to model the 3x3 grid.  Maybe "squares"?
-  # what data structure should we use?
-  # - array/hash of Square objects?
-  # - array/hash of strings or integers?
+    @squares = {}
+    (1..9).each { |key| @squares[key] = Square.new(INITIAL_MARKER) }
+  end
+
+  def get_square_at(key)
+    @squares[key]
+  end
+
+  def set_square_at(key, marker)
+    @squares[key].marker = marker
+  end
+
+  def unmarked_keys
+    @squares.keys.select { |key| @squares[key].unmarked? }
   end
 end
 
 class Square
-  def initialize
-  # maybe a "status" to keep track of this square's mark?
+  attr_accessor :marker
+
+  def initialize(marker)
+    @marker = marker
+  end
+
+  def to_s
+    @marker
+  end
+
+  def unmarked?
+    marker == Board::INITIAL_MARKER
   end
 end
 
 class Player
-  def initialize
-  # maybe a "marker" to keep track of this player's symbol (ie, 'X', or 'X')
-  end
-  
-  def mark
-    
+  attr_reader :marker
+
+  def initialize(marker)
+    @marker = marker
   end
 end
 
 class TTTGame
+  HUMAN_MARKER = "X"
+  COMPUTER_MARKER = "O"
+
+  attr_reader :board, :human, :computer
+
+  def initialize
+    @board = Board.new
+    @human = Player.new(HUMAN_MARKER)
+    @computer = Player.new(COMPUTER_MARKER)
+  end
+  def display_welcome_message
+    puts "Welcome to Tic Tac Toe!"
+    puts ""
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Tic Tac Toe! Goodbye!"
+  end
+
+  def display_board
+    puts ""
+    puts "     |     |"
+    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.
+    get_square_at(3)}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.
+    get_square_at(6)}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.
+    get_square_at(9)}"
+    puts "     |     |"
+    puts ""
+  end
+
+  def human_moves
+    puts "Choose a square (#{board.unmarked_keys.join(', ')}): "
+    square = nil
+    loop do
+      square = gets.chomp.to_i
+      break if board.unmarked_keys.include?(square)
+      puts "Sorry, that's not a valid choice."
+    end
+    board.set_square_at(square, human.marker)
+  end
+
+def computer_moves
+  board.set_square_at(board.unmarked_keys.sample, computer.marker)
+end
+
   def play
     display_welcome_message
+    display_board
     loop do
+      human_moves
+      # break if someone_won? || board_full?
+
+      computer_moves
+      # break if someone_won? || board_full?
+
       display_board
-      first_player_moves
-      break if someone_won? || board_full?
-      
-      second_player_moves
-      break if someone_won? || board_full?
     end
-    display_result
+    # display_result
     display_goodbye_message
   end
 end
