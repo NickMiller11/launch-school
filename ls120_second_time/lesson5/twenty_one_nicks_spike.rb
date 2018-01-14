edee60d6616f2615128015eed515d6845a91c7a3
Nick Miller <nick.miller00@gmail.com>
2018-01-14 09:31:01 -0800

    desk sync

--- ls120_second_time/lesson5/twenty_one_nicks_spike.rb
+++ ls120_second_time/lesson5/twenty_one_nicks_spike.rb
@@ -1,40 +1,3 @@
-=begin
-
-Twenty-One is a card game consisting of a dealer and a player, where the participants
-try to get as close to 21 as possible without going over.
-
-Here is an overview of the game:
-- Both participants are initially dealt 2 cards from a 52-card deck.
-- The player takes the first turn, and can "hit" or "stay".
-- If the player busts, he loses. If he stays, it's the dealer's turn.
-- The dealer must hit until his cards add up to at least 17.
-- If he busts, the player wins.  If both player and dealer stays, then the highest
-  total wins.
-- If both totals are equal, then it's a tie, and nobody wins.
-
-Nouns: player, dealer, card, deck, total, participant, game
-Verbs: busts, deal, hit, stay
-
-Participant
-  - hits
-  - stay
-  - busted
-  - total
-Dealer < Participant
-Player < Participant
-Card
-Deck
-  - deal
-Game
-  - start
-
-=end
-
-require 'pry'
-
-# working on dealer_turn method
-# need to add logic if dealer busts
-
 module Display
   def blank_line
     puts ""
@@ -56,6 +19,35 @@ module Display
 
   def display_you_busted_message
     puts "You have #{player.total} points. You busted!"
+  end
+
+  def display_dealer_busted_message
+    puts "Dealer has #{dealer.total} points. Dealer busted!"
+  end
+
+  def press_enter_to_continue
+    puts "Press enter to continue..."
+    gets.chomp
+  end
+
+  def display_final_score
+    puts "Final Score: Player - #{player.total} | Dealer - #{dealer.total}"
+  end
+
+  def show_result
+    if player.busted?
+      puts "The dealer won!  Better luck next time!"
+    elsif dealer.total > player.total
+      display_final_score
+      puts "The dealer won!  Better luck next time!"
+    elsif dealer.busted?
+      puts "You won!  Great job! You should trying playing at a casino..."
+    elsif dealer.total < player.total
+      display_final_score
+      puts "You won!  Great job! You should trying playing at a casino..."
+    else
+      puts "You tied!"
+    end
   end
 end
 
@@ -79,22 +71,31 @@ class Participant
     total > 21
   end
 
+  def reset_stay
+    self.stay = false
+  end
+
   def total
-    total = 0
+    total_points = 0
 
     hand.each do |card|
       case card.rank
-      when "Jack", "Queen", "King" then total += 10
-      when "Ace" then total += 11
+      when "Jack", "Queen", "King" then total_points += 10
+      when "Ace" then total_points += 11
       else
-        total += card.rank
-      end
-
-      hand.select { |card| card.rank == "Ace" }.count.times do
-        total -= 10 if total > 21
+        total_points += card.rank
       end
     end
-    total
+
+    hand.select { |card| card.rank == "Ace" }.count.times do
+      total_points -= 10 if total_points > 21
+    end
+
+    total_points
+  end
+
+  def discard_old_hand
+    self.hand = []
   end
 end
 
@@ -164,8 +165,9 @@ class Game
   def start
     display_welcome_message
     loop do
+      reset_stay_and_cards
       loop do
-        deal_cards # need to get rid of old cards if playing again
+        deal_cards
         show_initial_cards
         player_turn
         if player.busted?
@@ -173,8 +175,12 @@ class Game
           break
         end
         dealer_turn
-        binding.pry
-        show_result # not implemented
+        if dealer.busted?
+          display_dealer_busted_message
+          break
+        end
+        show_result
+        break
       end
       break unless play_again?
       clear_screen
@@ -183,7 +189,11 @@ class Game
 
   end
 
+  private
+
   def deal_cards
+    player.discard_old_hand
+    dealer.discard_old_hand
     deck.initial_deal(player.hand)
     deck.initial_deal(dealer.hand)
   end
@@ -206,14 +216,16 @@ class Game
       case move
       when 'h'
         deck.deal_one_card(player.hand)
+        clear_screen
         puts "You hit!"
+        blank_line
       when 's'
         player.stay!
+        clear_screen
         puts "You stay!"
+        blank_line
       end
-      blank_line
       show_cards
-      binding.pry
       break if player.stay? || player.busted?
     end
   end
@@ -221,10 +233,16 @@ class Game
   def dealer_turn
     loop do
       break if dealer.total > 17
+      clear_screen
       puts "Dealer hits!"
+      blank_line
       deck.deal_one_card(dealer.hand)
+      show_cards
+      press_enter_to_continue
     end
+    clear_screen
     puts "Dealer stays!"
+    blank_line
     dealer.stay
   end
 
@@ -262,8 +280,10 @@ class Game
     show_dealer_cards
   end
 
-  def both_stay?
-    player.stay? && dealer.stay?
+  def reset_stay_and_cards
+    player.reset_stay
+    dealer.reset_stay
+    self.deck = Deck.new
   end
 
   def play_again?
@@ -274,7 +294,7 @@ class Game
       break if ['y', 'n'].include?(answer)
       puts "I'm sorry, that's not a valid response."
     end
-    answer = 'y'
+    answer == 'y'
   end
 end
 
