1459e6c3849e9db4a7640b47697b21b99a4ac91a
Nick Miller <nick.miller00@gmail.com>
2018-02-05 20:27:40 +0000

    Started 2nd pass

---
+++ ls130_second_time/exercises/easy1/03_find_missing_num.rb
@@ -0,0 +1,25 @@
+# Write a method that takes a sorted array of integers as an argument,
+# and returns an array that includes all of the missing integers (in order)
+# between the first and last elements of the argument.
+
+# Examples:
+
+require 'pry'
+
+def missing(arr)
+  result = []
+  return result if arr.size == 1 || arr.empty?
+  arr.each_with_index do |num, index|
+    loop do
+      num += 1
+      break if num == arr[index + 1] || index == arr.size - 1
+      result << num
+    end
+  end
+  result
+end
+
+p missing([-3, -2, 1, 5]) == [-1, 0, 2, 3, 4]
+p missing([1, 2, 3, 4]) == []
+p missing([1, 5]) == [2, 3, 4]
+p missing([6]) == []
