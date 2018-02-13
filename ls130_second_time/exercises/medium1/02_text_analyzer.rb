b86a624119c3374715fac52d7819e25b2b3fdae2
Nick Miller <nick.miller00@gmail.com>
2018-02-13 23:49:01 +0000

    Medium1 exercises 2-5

--- ls130_second_time/exercises/medium1/02_text_analyzer.rb
+++ ls130_second_time/exercises/medium1/02_text_analyzer.rb
@@ -12,35 +12,20 @@
 
 class TextAnalyzer
   def process
-    # your implementation
+    file = File.open('sample_text.txt', 'r')
+    yield(file.read)
+    file.close
   end
 end
 
 analyzer = TextAnalyzer.new
-analyzer.process { puts # your implementation }
-analyzer.process { puts # your implementation }
-analyzer.process { puts # your implementation }
-
-# Sample Text File:
-
-# Eiusmod non aute commodo excepteur amet consequat ex elit. Ut excepteur ipsum
-# enim nulla aliqua fugiat quis dolore do minim non. Ad ex elit nulla commodo
-# aliqua eiusmod aliqua duis officia excepteur eiusmod veniam. Enim culpa laborum
-# nisi magna esse nulla ipsum ex consequat. Et enim et quis excepteur tempor ea
-# sit consequat cupidatat.
-
-# Esse commodo magna dolore adipisicing Lorem veniam quis ut labore pariatur quis
-# aliquip labore ad. Voluptate ullamco aliquip cillum cupidatat id in sint ipsum
-# pariatur nisi adipisicing exercitation id adipisicing qui. Nulla proident ad
-# elit dolore exercitation cupidatat mollit consequat enim cupidatat tempor
-# aliqua ea sunt ex nisi non.
-
-# Officia dolore labore non labore irure nisi ad minim consectetur non irure
-# veniam dolor. Laboris cillum fugiat reprehenderit elit consequat ullamco veniam
-# commodo.
+analyzer.process { |text| puts "#{text.split("\n\n").count} paragraphs" }
+analyzer.process { |text| puts "#{text.split("\n").count} lines" }
+analyzer.process { |text| puts "#{text.split(" ").count} words" }
 
 # Sample Output:
 
 # 3 paragraphs
 # 15 lines
 # 126 words
+
