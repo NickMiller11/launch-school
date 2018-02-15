8093249871886c4ad53c03999ef58b9235bf32bc
Nick Miller <nick.miller00@gmail.com>
2018-02-14 18:16:47 -0800

    Finished Todolist test suite assignment

--- ls130_second_time/lesson_2/todolist_test.rb
+++ ls130_second_time/lesson_2/todolist_test.rb
@@ -111,4 +111,66 @@ class TodoListTest < MiniTest::Test
     assert_equal(true, @todo3.done?)
   end
 
+  def test_remove_at
+    assert_raises(IndexError) { @list.remove_at(100) }
+
+    @list.remove_at(1)
+    assert_equal([@todo1, @todo3], @list.to_a)
+  end
+
+  def test_to_s_1
+    output = <<-OUTPUT.chomp.gsub /^\s+/, ""
+    ---- Today's Todos ----
+    [ ] Buy milk
+    [ ] Clean room
+    [ ] Go to gym
+    OUTPUT
+
+    assert_equal(output, @list.to_s)
+  end
+
+  def test_to_s_2
+    @todo1.done!
+    output = <<-OUTPUT.chomp.gsub /^\s+/, ""
+    ---- Today's Todos ----
+    [X] Buy milk
+    [ ] Clean room
+    [ ] Go to gym
+    OUTPUT
+
+    assert_equal(output, @list.to_s)
+  end
+
+  def test_to_s_3
+    @todo1.done!
+    @todo2.done!
+    @todo3.done!
+    output = <<-OUTPUT.chomp.gsub /^\s+/, ""
+    ---- Today's Todos ----
+    [X] Buy milk
+    [X] Clean room
+    [X] Go to gym
+    OUTPUT
+
+    assert_equal(output, @list.to_s)
+  end
+
+  def test_each_1
+    @list.each { |todo| todo.done! }
+    assert_equal(true, @list.done?)
+  end
+
+  def test_each_2
+    result = []
+    actual_test = @list.each { |todo| result << todo }
+    assert_equal(@list, actual_test)
+  end
+
+  def test_select
+    @todo1.done!
+    @todo3.done!
+    test = @list.select { |todo| todo.done? }.to_a
+    assert_equal([@todo1, @todo3], test)
+    refute_equal(@list, test)
+  end
 end
