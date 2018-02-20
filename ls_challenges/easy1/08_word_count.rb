77df6495129b99949643a0b56f3da422728d8e71
Nick Miller <nick.miller00@gmail.com>
2018-02-20 20:14:42 +0000

    Exercise 9 done

--- ls_challenges/easy1/08_word_count.rb
+++ ls_challenges/easy1/08_word_count.rb
@@ -25,26 +25,81 @@ algorithm:
 
 =end
 
-class Phrase
+#LS Solution
 
-  def initialize(words)
-    @words = words
+class Phrase
+  def initialize(phrase)
+   @words = phrase.split(/[\s,]/)
+                  .select { |word| word.match(/[a-zA-Z0-9]/) } # also match.(/[[[:alnum:]]]/)
+                  .map { |word| transform_word(word) }
+  # can also use - @words.scan(/\w+'?\w|\d+/) <-- what does this do??
+
   end
 
   def word_count
-    words_arr = prepare_string(@words)
-    words_arr.each_with_object({}) do |word, hash|
-      if hash.keys.include?(word)
-        hash[word] += 1
-      else
-        hash[word] = 1
-      end
+    summary = Hash.new(0)
+    @words.each do |word|
+      summary[word] += 1
     end
+    summary
+  end
+
+  def transform_word(word)
+    word.gsub(/[^a-zA-Z0-9']|^'|'$/, '').downcase
+  end
+end
+
+# Very simple student solution
+
+Class Phrase
+  def initialize(sentence)
+    @sentence = sentence.downcase.scan(/[a-z0-9']+/)
+  end
+
+  def word_count
+    remove_quotes.each_with_object(Hash.new(0)) { |word, hash| hash[word] += 1 }
   end
 
   private
 
-  def prepare_string(words)
-    words.downcase.scan(/[\w']+/)
-  end
-end
+  def remove_quotes
+    @sentence.each { |word| word.gsub!(/^'|'$/, '')
+
+=begin
+
+- I had a lot of trouble with this one, I couldn't seem to get the Regex structured
+  correctly...I was able to hit all the edge cases but the last one (apostrophe
+  within words)
+
+Notes from video:
+
+- Look at the test cases first before I start coding to check all the requirements
+  but don't worry about the edge cases at first
+
+=end
+
+# My first attempt
+
+# class Phrase
+
+#   def initialize(words)
+#     @words = words
+#   end
+
+#   def word_count
+#     words_arr = prepare_string(@words)
+#     words_arr.each_with_object({}) do |word, hash|
+#       if hash.keys.include?(word)
+#         hash[word] += 1
+#       else
+#         hash[word] = 1
+#       end
+#     end
+#   end
+
+#   private
+
+#   def prepare_string(words)
+#     words.downcase.gsub(/[.,:;!@#$%\^&*\(\)]+('[^'])/, ' ').squeeze(' ').split(' ')
+#   end
+# end
