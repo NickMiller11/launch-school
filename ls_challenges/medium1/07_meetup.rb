508699afe7d6430869923797b42e30bab1460fc5
Nick Miller <nick.miller00@gmail.com>
2018-03-05 20:58:22 -0800

    Second attempt and refactoring

--- ls_challenges/medium1/07_meetup.rb
+++ ls_challenges/medium1/07_meetup.rb
@@ -15,6 +15,54 @@
 #
 # Write code that calculates date of meetups.
 
+###################### Second try ###########################
+
+=begin
+
+input: the day method takes in a day of the week and 'schedule'
+output: date object
+
+rules:
+based on the day and schedule passed into the day method, calculate
+the exact date of the meetup given the month and year states of the
+Meetup object
+
+algorithm:
+- create a range of date objects from the value in the day_range hash to
+that value plus 7
+- use detect to return the date object that matches the weekday passed
+into the day method
+
+=end
+
+require 'date'
+
+DAYS_OF_WEEK = [:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday]
+
+class Meetup
+
+  def initialize(month, year)
+    @month = month
+    @year = year
+    @day_range = {
+      first: 1,
+      second: 8,
+      third: 15,
+      fourth: 22,
+      teenth: 13,
+      last: -7
+    }
+  end
+
+  def day(day, schedule)
+    start_date = @day_range[schedule]
+    date_range = Date.new(@year, @month, start_date)..Date.new(@year, @month, start_date + 6)
+    date_range.detect { |date| date.wday == DAYS_OF_WEEK.index(day) }
+  end
+end
+
+###################### First try ###########################
+
 =begin
 
 input: symbol for weekday, symbol for position of weekday in month
@@ -43,57 +91,57 @@ algorithm:
 
 =end
 
-require 'date'
-
-class Meetup
-  MONTH_RANGES =  {
-                  first: (1..7),
-                  second: (8..14),
-                  teenth: (13..19),
-                  third: (15..21),
-                  fourth: (22..29),
-                  last_28: (22..28),
-                  last_30: (24..30),
-                  last_31: (25..31)
-                  }
-
-  WEEKDAYS =      {
-                  monday: 1,
-                  tuesday: 2,
-                  wednesday: 3,
-                  thursday: 4,
-                  friday: 5,
-                  saturday: 6,
-                  sunday: 7
-                  }
-
-  def initialize(month, year)
-    @month = month
-    @year = year
-  end
-
-  def day(weekday, schedule)
-    if schedule == :last && Date.new(@year, @month, -1).mday == 28
-      find_correct_date(weekday, :last_28)
-    elsif schedule == :last && Date.new(@year, @month, -1).mday == 30
-      find_correct_date(weekday, :last_30)
-    elsif schedule == :last && Date.new(@year, @month, -1).mday == 31
-      find_correct_date(weekday, :last_31)
-    else
-      find_correct_date(weekday, schedule)
-    end
-  end
-
-  private
-
-  def find_correct_date(weekday, schedule)
-    MONTH_RANGES[schedule].each do |date|
-      test_date = Date.new(@year, @month, date)
-      return test_date if test_date.cwday == WEEKDAYS[weekday]
-    end
-  end
-
-end
+# require 'date'
+#
+# class Meetup
+#   MONTH_RANGES =  {
+#                   first: (1..7),
+#                   second: (8..14),
+#                   teenth: (13..19),
+#                   third: (15..21),
+#                   fourth: (22..29),
+#                   last_28: (22..28),
+#                   last_30: (24..30),
+#                   last_31: (25..31)
+#                   }
+#
+#   WEEKDAYS =      {
+#                   monday: 1,
+#                   tuesday: 2,
+#                   wednesday: 3,
+#                   thursday: 4,
+#                   friday: 5,
+#                   saturday: 6,
+#                   sunday: 7
+#                   }
+#
+#   def initialize(month, year)
+#     @month = month
+#     @year = year
+#   end
+#
+#   def day(weekday, schedule)
+#     if schedule == :last && Date.new(@year, @month, -1).mday == 28
+#       find_correct_date(weekday, :last_28)
+#     elsif schedule == :last && Date.new(@year, @month, -1).mday == 30
+#       find_correct_date(weekday, :last_30)
+#     elsif schedule == :last && Date.new(@year, @month, -1).mday == 31
+#       find_correct_date(weekday, :last_31)
+#     else
+#       find_correct_date(weekday, schedule)
+#     end
+#   end
+#
+#   private
+#
+#   def find_correct_date(weekday, schedule)
+#     MONTH_RANGES[schedule].each do |date|
+#       test_date = Date.new(@year, @month, date)
+#       return test_date if test_date.cwday == WEEKDAYS[weekday]
+#     end
+#   end
+#
+# end
 
 =begin
 
