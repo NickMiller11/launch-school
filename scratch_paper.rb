5f6d76bc8f64bd0202e8f761cdbfea0d6ffc7e8f
Nick Miller <nick.miller00@gmail.com>
2018-03-07 17:48:31 +0000

    regex exercises

--- scratch_paper.rb
+++ scratch_paper.rb
@@ -1,34 +1,7 @@
-
-=begin
-
-input:
-output:
-
-rules:
-- use 3 enumerator methods
-- print the first 7 factorials using an external iterator
-  - once done, reset the enumerator
-  - repreint the same 7 factorials using enumerator object as internal iterator
-
-algorithm:
-
-=end
-
-factorial = Enumerator.new do |yielder|
-  accumulator = 1
-  number = 0
-  loop do
-    accumulator = number.zero? ? 1 : accumulator * number
-    yielder << accumulator
-    number += 1
-  end
+def format_date(str)
+  str.sub(/\A(\d\d\d\d)[\-\/](\d\d)[\-\/](\d\d)\z/, '\3.\2.\1')
 end
 
-7.times { puts factorial.next }
 
-factorial.rewind
-
-factorial.each_with_index do |number, index|
-  puts number
-  break if index == 6
-end
+p format_date('2016-06-17') # -> '17.06.2016'
+p format_date('2016/06/17') # -> '2016/06/17' (no change)
