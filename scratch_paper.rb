c33342f00006e85e4e72d0b093e3cf5b1844861a
Nick Miller <nick.miller00@gmail.com>
2018-03-07 15:25:10 -0800

    Practice exercises

--- scratch_paper.rb
+++ scratch_paper.rb
@@ -1,7 +1,9 @@
-def format_date(str)
-  str.sub(/\A(\d\d\d\d)[\-\/](\d\d)[\-\/](\d\d)\z/, '\3.\2.\1')
+def divisors(num)
+  (1..num**0.5).flat_map { |n| [n, num / n] if (num % n).zero? }.compact.uniq.sort
 end
 
-
-p format_date('2016-06-17') # -> '17.06.2016'
-p format_date('2016/06/17') # -> '2016/06/17' (no change)
+p divisors(1) == [1]
+p divisors(7) == [1, 7]
+p divisors(12) == [1, 2, 3, 4, 6, 12]
+p divisors(98) == [1, 2, 7, 14, 49, 98]
+p divisors(99400891) == [1, 9967, 9973, 99400891] # may take a minute
