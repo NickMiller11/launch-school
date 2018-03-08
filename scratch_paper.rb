0cd97c5d5bb886a99ef4fcc321877849f9c75e92
Nick Miller <nick.miller00@gmail.com>
2018-03-08 12:39:32 -0800

    Additional exercise practice

--- scratch_paper.rb
+++ scratch_paper.rb
@@ -1,19 +1,15 @@
+def drop_while(arr)
+  arr.each_with_index do |el, index|
+    return arr[index..-1] if !yield(el)
+  end
+  []
+end
 
-# def map(arr)
-#   arr.each_with_object([]) do |value, object|
-#     object << yield(value)
-#   end
-# end
 
-# def map(arr)
-#   arr.reduce([]) do |result, value|
-#     result << yield(value)
-#   end
-# end
 
-p map([1, 3, 6]) { |value| value**2 } == [1, 9, 36]
-p map([]) { |value| true } == []
-p map(['a', 'b', 'c', 'd']) { |value| false } == [false, false, false, false]
-p map(['a', 'b', 'c', 'd']) { |value| value.upcase } == ['A', 'B', 'C', 'D']
-p map([1, 3, 4]) { |value| (1..value).to_a } == [[1], [1, 2, 3], [1, 2, 3, 4]]
-
+p drop_while([1, 3, 5, 6]) { |value| value.odd? } == [6]
+p drop_while([1, 3, 5, 6]) { |value| value.even? } == [1, 3, 5, 6]
+p drop_while([1, 3, 5, 6]) { |value| true } == []
+p drop_while([1, 3, 5, 6]) { |value| false } == [1, 3, 5, 6]
+p drop_while([1, 3, 5, 6]) { |value| value < 5 } == [5, 6]
+p drop_while([]) { |value| true } == []
