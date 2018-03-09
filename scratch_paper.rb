df0ea77b6fc06aeebf7a5b45c38ba2f0de2f470d
Nick Miller <nick.miller00@gmail.com>
2018-03-09 20:05:35 +0000

    Practice exercises

--- scratch_paper.rb
+++ scratch_paper.rb
@@ -1,41 +1,140 @@
-# def each_cons(arr)
-#   arr[0..-2].each_with_index { |value, index| yield(value, arr[index + 1]) } ; nil
-# end
+# This class represents a todo item and its associated
+# data: name and description. There's also a "done"
+# flag to show whether this todo item is done.
 
-# def each_cons(arr)
-#   index = 0
-#   while index < arr.size - 1
-#     yield(arr[index], arr[index + 1])
-#     index += 1
-#   end
-# end
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
 
-hash = {}
-each_cons([1, 3, 6, 10], 1) do |value|
-  hash[value] = true
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
 end
-p hash == { 1 => true, 3 => true, 6 => true, 10 => true }
 
-hash = {}
-each_cons([1, 3, 6, 10], 2) do |value1, value2|
-  hash[value1] = value2
-end
-p hash == { 1 => 3, 3 => 6, 6 => 10 }
+# This class represents a collection of Todo objects.
+# You can perform typical collection-oriented actions
+# on a TodoList object, including iteration and selection.
 
-hash = {}
-each_cons([1, 3, 6, 10], 3) do |value1, *values|
-  hash[value1] = values
-end
-p hash == { 1 => [3, 6], 3 => [6, 10] }
+class TodoList
+  attr_accessor :title
 
-hash = {}
-each_cons([1, 3, 6, 10], 4) do |value1, *values|
-  hash[value1] = values
-end
-p hash == { 1 => [3, 6, 10] }
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
 
-hash = {}
-each_cons([1, 3, 6, 10], 5) do |value1, *values|
-  hash[value1] = values
+  def size
+    @todos.size
+  end
+
+  def first
+    @todos.first
+  end
+
+  def last
+    @todos.last
+  end
+
+  def shift
+    @todos.shift
+  end
+
+  def pop
+    @todos.pop
+  end
+
+  def done?
+    @todos.all? { |todo| todo.done? }
+  end
+
+  def <<(todo)
+    raise TypeError, 'can only add Todo objects' unless todo.instance_of? Todo
+
+    @todos << todo
+  end
+  alias_method :add, :<<
+
+  def item_at(idx)
+    @todos.fetch(idx)
+  end
+
+  def mark_done_at(idx)
+    item_at(idx).done!
+  end
+
+  def mark_undone_at(idx)
+    item_at(idx).undone!
+  end
+
+  def done!
+    @todos.each_index do |idx|
+      mark_done_at(idx)
+    end
+  end
+
+  def remove_at(idx)
+    @todos.delete(item_at(idx))
+  end
+
+  def to_s
+    text = "---- #{title} ----\n"
+    text << @todos.map(&:to_s).join("\n")
+    text
+  end
+
+  def to_a
+    @todos
+  end
+
+  def each
+    counter = 0
+    while counter < @todos.size do
+      yield(@todos[counter])
+      counter += 1
+    end
+    self
+  end
+
+  def select
+    result = TodoList.new("Result")
+    each do |todo|
+      result << todo if yield(todo)
+    end
+    result
+  end
 end
-p hash == {}
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+
+list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+
+todo1.done!
+
+results = list.select { |todo| todo.done? }    # you need to implement this method
+
+puts results.inspect
