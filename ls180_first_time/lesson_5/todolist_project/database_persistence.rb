8ee3c4eb22e7eb13a7320cb636e39437eb2aaff8
Nick Miller <nick.miller00@gmail.com>
2018-06-01 22:41:55 +0000

    changing settings

--- ls180_first_time/lesson_5/todolist_project/database_persistence.rb
+++ ls180_first_time/lesson_5/todolist_project/database_persistence.rb
@@ -2,8 +2,16 @@ require "pg"
 
 class DatabasePersistence
   def initialize(logger)
-    @db = PG.connect(dbname: "todos")
-    @logger = logger
+    @db = if Sinatra::Base.production?
+            PG.connect(ENV['DATABASE_URL'])
+          else
+            PG.connect(dbname: "todos")
+            @logger = logger
+          end
+  end
+
+  def disconnect
+    @db.close
   end
 
   def query(statement, *params)
