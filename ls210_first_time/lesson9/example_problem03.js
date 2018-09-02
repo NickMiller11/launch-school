874b44c31ebfe5348d01794f8d25d3b98b86b38a
Nick Miller <nick.miller00@gmail.com>
2018-09-02 06:51:48 -0700

    Working through 210

--- ls210_first_time/lesson9/example_problem03.js
+++ ls210_first_time/lesson9/example_problem03.js
@@ -25,10 +25,75 @@ algorithm:
 */
 
 function convertShorthandNumbers(numberString) {
+  var completeNumbers = [];
+  var numberArray = numberString.split(',');
+  var i;
+  var currentValue;
+  var previousValue;
+  var fullRange;
 
+  for (i = 0; i < numberArray.length; i += 1) {
+    currentValue = numberArray[i];
+    previousValue = completeNumbers[completeNumbers.length - 1];
+
+    if (currentValue.includes('-') || currentValue.includes(':') || currentValue.includes('..')) {
+      fullRange = expandRange(currentValue, previousValue);
+
+      fullRange.forEach(function (num) {
+        completeNumbers.push(num);
+      });
+    } else if (currentValue < previousValue) {
+      completeNumbers.push(adjustNumber(currentValue, previousValue));
+    } else {
+      completeNumbers.push(parseInt(currentValue, 10));
+    }
+  }
+
+  return completeNumbers;
 }
 
-console.log(convertShorthandNumbers("1,3,7,2,4,1")); // [1, 3, 7, 12, 14, 21]
+function adjustNumber(currentValue, previousValue) {
+  currentValue = parseInt(currentValue, 10);
+  previousValue = parseInt(previousValue, 10);
+
+  while (currentValue < previousValue) {
+    currentValue += 10;
+  }
+
+  return currentValue;
+}
+
+function expandRange(currentValue, previousValue) {
+  var numberRange = [];
+  var tempNumber;
+
+  previousValue = parseInt(previousValue, 10);
+
+  if (currentValue.startsWith(' ')) {
+    currentValue = currentValue.slice(1);
+  }
+
+  currentValue = currentValue.split(/\D/).map(function (numString) {
+    return parseInt(numString, 10);
+  });
+
+  currentValue = currentValue.map(function (num) {
+    return adjustNumber(num, previousValue);
+  });
+
+  tempNumber = currentValue[0];
+
+  while (tempNumber <= currentValue[currentValue.length - 1]) {
+    numberRange.push(tempNumber);
+    tempNumber += 1;
+  }
+
+  return numberRange;
+}
+
+
+
+// console.log(convertShorthandNumbers("1,3,7,2,4,1")); // [1, 3, 7, 12, 14, 21]
 console.log(convertShorthandNumbers("1-3, 1-2")); // [1, 2, 3, 11, 12]
 console.log(convertShorthandNumbers("1:5:2")); // [1, 2, 3, 4, 5, 6, ... 12]
 console.log(convertShorthandNumbers("104-2")); // [104, 105, ... 112]
