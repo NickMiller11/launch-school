7b5cf305daea55bd269f69f0a92529fe298adde6
Nick Miller <nick.miller00@gmail.com>
2018-09-05 14:31:52 +0000

    Working on 210

--- ls210_second_time/small_problems/text_string_processing/problem10.js
+++ ls210_second_time/small_problems/text_string_processing/problem10.js
@@ -1,4 +1,11 @@
+// The function from the previous exercise returns the number of occurrences of a
+// word in some text. Although this is useful, there are also situations in which
+// we just want to find the word in the context of the text.
 
+// For this exercise, write a function that takes a word and a string of text as
+// arguments, and returns the text with every instance of the word highlighted.
+// To highlight a word, enclose the word with two asterisks ('**') on each side
+// and change every letter of the word to uppercase (e.g., '**HIGHLIGHTEDWORD**').
 
 var text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem ' +
            'accusantium doloremque laudantium, totam rem aperiam, eaque ipsa ' +
@@ -17,8 +24,9 @@ var text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem ' +
 
 
 function searchWord(word, text) {
-  word = new RegExp(word, "gi");
-  return text.match(word).length;
+  wordRegex = new RegExp(word, "gi");
+
+  return text.replace(wordRegex, '**' + word.toUpperCase() + '**');
 }
 
 console.log(searchWord('sed', text));      // 3
