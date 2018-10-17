4259958707b563d1a0985cd2c019b2717585d195
Nick Miller <nick.miller00@gmail.com>
2018-10-17 19:04:20 +0000

    Working on 230

---
+++ ls230_first_time/lesson3/calculator.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $("form").on("submit", function(e) {
+    var $form = $(this);
+    var numerator = +$form.find("#numerator").val();
+    var denominator = +$form.find("#denominator").val();
+    var operator = $form.find("#operator").val();
+    var result = 0;
+
+    e.preventDefault();
+
+    if (operator === "+") {
+      result = numerator + denominator;
+    }
+
+    if (operator === "-") {
+      result = numerator - denominator;
+    }
+
+    if (operator === "*") {
+      result = numerator * denominator;
+    }
+
+    if (operator === "/") {
+      result = numerator / denominator;
+    }
+
+    $("h1").text(result);
+  });
+});
---
+++ ls230_first_time/lesson3/grocery_list.js
@@ -0,0 +1,3 @@
+$(function() {
+  var $ul = $("ul");
+});
